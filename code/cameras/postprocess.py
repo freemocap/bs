8932df6ff00a51c988d663171900a9221f7a26ed
philipqueen <queen.philip@gmail.com>
2025-02-07 15:01:52 -0700

    add post process pipeline script

---
+++ code/cameras/postprocess.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+from cameras.synchronization.timestamp_synchronize import TimestampSynchronize
+from cameras.synchronization.pupil_synch import PupilSynchronize
+from video_viewing.combine_videos import combine_videos, create_video_info
+
+
+def main(session_folder_path: Path):
+    """
+    Postprocess a session folder
+    
+    Folder should contain Basler videos, timestamps, and timestamp map in a folder titled `raw_videos`
+    as well as pupil data in a folder titled `pupil_output`
+    It will synchronize the Basler videos, then synchronize them with the pupil videos, and then combine the videos into a single video
+    """
+
+    timestamp_synchronize = TimestampSynchronize(session_folder_path)
+    timestamp_synchronize.synchronize()
+
+    pupil_synchronize = PupilSynchronize(session_folder_path)
+    pupil_synchronize.synchronize()
+
+    combined_data_path = session_folder_path / "basler_pupil_synchronized"
+
+    basler_videos, pupil_videos = create_video_info(folder_path=combined_data_path)
+
+    combine_videos(basler_videos=basler_videos, pupil_videos=pupil_videos)
+
+
+if __name__ == "__main__":
+    session_folder_path = Path(
+        "/Users/philipqueen/Documents/Humon Research Lab/Basler Stuff/fabio_hand/"
+    )
+
+    main(session_folder_path)
