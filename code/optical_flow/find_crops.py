93c80df59c5eab5fcb1ef4cf630db9946b23c97a
philipqueen <queen.philip@gmail.com>
2025-03-12 21:41:48 -0600

    optical flow updates

--- code/optical_flow/find_crops.py
+++ code/optical_flow/find_crops.py
@@ -1,3 +1,4 @@
+from json import JSONDecodeError
 from typing import Optional, Tuple, Sequence
 import cv2
 import numpy as np
@@ -13,8 +14,17 @@ def find_crops(image: np.ndarray) -> Tuple[int, int, int, int]:
 
 
 if __name__ == "__main__":
-    pupil_video_path = "/Users/philipqueen/ferret_0776_P35_EO5/basler_pupil_synchronized/eye0.mp4"
-    cap = cv2.VideoCapture(pupil_video_path)
+    from pathlib import Path
+    from pupil_info import JSON_PATH, load_json, PupilInfo, save_models_to_json, recording_id_from_path
+
+    pupil_video_path = Path("/Users/philipqueen/session_2024-12-18/ferret_0776_P44_E14/eye1.mp4")
+    cap = cv2.VideoCapture(str(pupil_video_path))
     ret, frame = cap.read()
-    find_crops(frame)
+    crops = find_crops(frame)
     cap.release()
+    try:
+        pupil_info_dict = load_json(Path(JSON_PATH))
+    except JSONDecodeError:
+        pupil_info_dict = {}
+    pupil_info_dict[recording_id_from_path(pupil_video_path)] = PupilInfo.from_path_and_crop(path=pupil_video_path, crop=crops)
+    save_models_to_json(pupil_info_dict, Path(JSON_PATH))
--- code/optical_flow/run_simple_optical_flow.py
+++ code/optical_flow/run_simple_optical_flow.py
@@ -2,41 +2,64 @@ from typing import Optional, Tuple
 import numpy as np
 import cv2 as cv
 
-def run_simple_optical_flow(cap: cv.VideoCapture, crop: Optional[Tuple[int, int, int, int]] = None, display: bool = False):
+def run_simple_optical_flow(cap: cv.VideoCapture, crop: Optional[Tuple[int, int, int, int]] = None, display: bool = True, record: bool = False):
     if crop is not None:
         x, y, w, h = crop
     else:
         x, y, w, h = 0, 0, int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+    if record:
+        output_pathstring = "output.mp4"
+        fourcc = cv.VideoWriter.fourcc(*"mp4v")
+        writer = cv.VideoWriter(output_pathstring, fourcc, cap.get(cv.CAP_PROP_FPS), (int(2*w), int(h)))
+
+
     ret, frame1 = cap.read()
     if not ret:
         raise Exception("Failed to read video")
-    previous = cv.cvtColor(frame1[x:x+w, y:y+h], cv.COLOR_BGR2GRAY)
-    hsv = np.zeros_like(frame1[x:x+w, y:y+h])
+    frame1 = frame1[y:y+h, x:x+w]
+    previous = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
+    hsv = np.zeros_like(frame1)
     hsv[..., 1] = 255
     while True:
         ret, frame2 = cap.read()
         if not ret:
             print('No frames grabbed!')
             break
-        next = cv.cvtColor(frame2[x:x+w, y:y+h], cv.COLOR_BGR2GRAY)
-        flow = cv.calcOpticalFlowFarneback(previous, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-        mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
-        hsv[..., 0] = ang*180/np.pi/2
-        hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+        frame2 = frame2[y:y+h, x:x+w]
+        next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
+        flow = cv.calcOpticalFlowFarneback(previous, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)  # magic values come from OpenCV docs
+        magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
+        hsv[..., 0] = angle*180/np.pi/2
+        hsv[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
         bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
-        combined = np.concatenate((frame2[x:x+w, y:y+h], bgr), axis=1)
+        combined = np.concatenate((frame2, bgr), axis=1)
         if display:
             cv.imshow('frame2', combined)
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
             previous = next
+        if record:
+            writer.write(combined)
     if display:
         cv.destroyAllWindows()
+    if record:
+        print(f"saving video to {output_pathstring}")
+        writer.release()
 
     cap.release()
 
 if __name__ == "__main__":
-    pupil_video_path = "/Users/philipqueen/ferret_0776_P35_EO5/basler_pupil_synchronized/eye0.mp4"
-    crop = (224, 194, 85, 87)
-    cap = cv.VideoCapture(pupil_video_path)
-    run_simple_optical_flow(cap, crop=crop, display=True)
+    from pathlib import Path
+    from pupil_info import JSON_PATH, load_json, recording_id_from_path
+
+    pupil_video_path = Path("/Users/philipqueen/session_2024-12-18/ferret_0776_P44_E14/eye1.mp4")
+
+    pupil_info_dict = load_json(Path(JSON_PATH))
+    if recording_id_from_path(pupil_video_path) in pupil_info_dict:
+        crop = pupil_info_dict[recording_id_from_path(pupil_video_path)].crop
+    else:
+        raise Exception("Model not found in JSON file, please run find_crops.py for this recording")
+
+    cap = cv.VideoCapture(str(pupil_video_path))
+    run_simple_optical_flow(cap, crop=crop, display=False, record=True)
