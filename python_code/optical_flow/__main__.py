cc482a3b96d9a0d9391d76eeb3840ac777009672
philipqueen <queen.philip@gmail.com>
2025-03-13 13:02:03 -0600

    add option for recalculating optical flow crops

--- python_code/optical_flow/__main__.py
+++ python_code/optical_flow/__main__.py
@@ -6,11 +6,11 @@ from python_code.optical_flow.dense_optical_flow import dense_optical_flow
 from python_code.optical_flow.find_crops import find_and_save_crops
 
 
-def main(pupil_video_path: Path | str):
+def main(pupil_video_path: Path | str, force_recalculate_crops: bool = False):
     pupil_video_path = Path(pupil_video_path)
 
     pupil_info_dict = load_json(Path(JSON_PATH))
-    if recording_id_from_path(pupil_video_path) in pupil_info_dict:
+    if recording_id_from_path(pupil_video_path) in pupil_info_dict and not force_recalculate_crops:
         crop = pupil_info_dict[recording_id_from_path(pupil_video_path)].crop
     else:
         crop = find_and_save_crops(pupil_video_path)
@@ -30,4 +30,4 @@ if __name__ == "__main__":
         "/Users/philipqueen/session_2024-12-18/ferret_0776_P44_E14/eye1.mp4"
     )
 
-    main(pupil_video_path=pupil_video_path)
+    main(pupil_video_path=pupil_video_path, force_recalculate_crops=False)
