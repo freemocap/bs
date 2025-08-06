1efcaad8f17511e95998f3d84551c6c7dab427da
bs_lab <info@freemocap.org>
2025-08-06 11:34:13 -0600

    add useful name=main to intrinsics corrector

--- python_code/cameras/intrinsics/intrinsics_corrector.py
+++ python_code/cameras/intrinsics/intrinsics_corrector.py
@@ -15,7 +15,7 @@ class IntrinsicsCorrector:
     height: int
 
     def __post_init__(self):
-        include_all_pixels = 0  # 1: all pixels are retained with some extra black in margins, 0: only valid pixels are shown
+        include_all_pixels = 1  # 1: all pixels are retained with some extra black in margins, 0: only valid pixels are shown
         # using 0 preserves image size
 
         self.new_camera_matrix, _roi = cv2.getOptimalNewCameraMatrix(
@@ -57,3 +57,24 @@ def get_calibrations_from_json(json_path: Path = INTRINSICS_JSON_PATH) -> dict[s
         }
 
     return data
+
+if __name__ == "__main__":
+    video = Path("/home/scholl-lab/recordings/session_2025-06-28/ferret_757_EyeCameras_P30_EO2/raw_videos/25006505.mp4")
+
+    intrinsic_data = get_calibrations_from_json()
+
+    intrinsics = intrinsic_data[video.stem]
+
+    cap = cv2.VideoCapture(str(video))
+
+    corrector = IntrinsicsCorrector.from_dict(dict=intrinsics, width=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+    ret, frame = cap.read()
+
+    if not ret:
+        raise ValueError("Unable to read frame")
+    
+    corrected_frame = corrector.correct_frame(frame)
+
+    cv2.imshow("corrected_frame", corrected_frame)
+    cv2.waitKey(0)
