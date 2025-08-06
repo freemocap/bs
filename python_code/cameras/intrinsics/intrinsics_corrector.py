2c906b96a0aef896ec6758161466d650a4f38181
bs_lab <info@freemocap.org>
2025-08-05 18:04:03 -0600

    fix image sizing for corrected images

--- python_code/cameras/intrinsics/intrinsics_corrector.py
+++ python_code/cameras/intrinsics/intrinsics_corrector.py
@@ -15,10 +15,10 @@ class IntrinsicsCorrector:
     height: int
 
     def __post_init__(self):
-        include_all_pixels = 1  # 1: all pixels are retained with some extra black in margins, 0: only valid pixels are shown
-        # we can do 1 here because we crop it down anyways
+        include_all_pixels = 0  # 1: all pixels are retained with some extra black in margins, 0: only valid pixels are shown
+        # using 0 preserves image size
 
-        self.new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(
+        self.new_camera_matrix, _roi = cv2.getOptimalNewCameraMatrix(
             self.camera_matrix,
             self.distortion_coefficients,
             (self.width, self.height),
@@ -26,7 +26,6 @@ class IntrinsicsCorrector:
             (self.width, self.height),
         )
 
-        self.x, self.y, self.w, self.h = roi
 
     @classmethod
     def from_dict(cls, dict: dict[str, np.ndarray], width: int, height: int) -> "IntrinsicsCorrector":
@@ -40,7 +39,12 @@ class IntrinsicsCorrector:
     def correct_frame(self, frame: np.ndarray) -> np.ndarray:
         corrected_image = cv2.undistort(frame, self.camera_matrix, self.distortion_coefficients, None, self.new_camera_matrix)
 
-        return corrected_image[self.y : self.y + self.h, self.x : self.x + self.w]
+        cropped_image = corrected_image
+
+        if cropped_image.shape != frame.shape:
+            raise ValueError(f"Shape changed! original shape was {frame.shape} but corrected image is {cropped_image.shape}")
+
+        return cropped_image
     
 def get_calibrations_from_json(json_path: Path = INTRINSICS_JSON_PATH) -> dict[str, dict[str, np.ndarray]]:
     with open(json_path, "r") as f:
