d5a620ebb9e98a75cb51b5ef507326c366425661
bs_lab <info@freemocap.org>
2025-09-16 17:05:26 -0600

    improve finding closest pupil frame to basler frame (better diagnostics)

--- python_code/video_viewing/closest_pupil_frame_to_basler_frame.py
+++ python_code/video_viewing/closest_pupil_frame_to_basler_frame.py
@@ -1,8 +1,7 @@
 import numpy as np
 from pathlib import Path
 
-if __name__=='__main__':
-    session_folder = Path("/home/scholl-lab/ferret_recordings/session_2025-07-01_ferret_757_EyeCameras_P33_EO5")
+def closest_pupil_frame_to_basler_frame(session_folder, starting_basler_frame, ending_basler_frame):
     full_recording_folder = session_folder / "full_recording"
     eye_videos_folder = full_recording_folder / "eye_data" / "eye_videos"
     mocap_videos_folder = full_recording_folder / "mocap_data" / "synchronized_corrected_videos"
@@ -12,8 +11,6 @@ if __name__=='__main__':
     # print([file.name for file in basler_timestamp_files])
     basler_timestamp_arrays = [np.load(timestamp_path) for timestamp_path in basler_timestamp_files]
 
-    starting_basler_frame = 7200
-    ending_basler_frame = 12600
 
     basler_median_starting_timestamp = np.median([timestamps[starting_basler_frame] for timestamps in basler_timestamp_arrays])
     basler_median_ending_timestamp = np.median([timestamps[ending_basler_frame] for timestamps in basler_timestamp_arrays])
@@ -22,12 +19,19 @@ if __name__=='__main__':
     print(f"Median basler time at frame {ending_basler_frame} is {int(basler_median_ending_timestamp)} ns")
 
     print(f"Time difference between Basler start and end is {basler_median_ending_timestamp - basler_median_starting_timestamp} ns")
+    clip_info = {"basler": 
+                 {"start_frame": int(starting_basler_frame), 
+                  "end_frame": int(ending_basler_frame),
+                  "start time_ns": int(basler_median_starting_timestamp),
+                  "end_time_ns": int(basler_median_ending_timestamp)}
+    }
     for eye in ("eye0", "eye1"):
         pupil_timestamps = np.load(eye_videos_folder / f"{eye}_timestamps_utc.npy")
         print(f"first pupil timestamp from {eye}; {pupil_timestamps[0]}")
         print(f"last pupil timestamp from {eye}; {pupil_timestamps[-1]}")
 
-
+        frame_numbers = []
+        times = []
         for basler_frame_number, basler_time in zip([starting_basler_frame, ending_basler_frame], [basler_median_starting_timestamp, basler_median_ending_timestamp]):
             estimated_match = np.searchsorted(pupil_timestamps, basler_time, side="right")
             if (basler_frame_number - pupil_timestamps[estimated_match-1]) < abs(basler_frame_number - pupil_timestamps[estimated_match]):
@@ -35,4 +39,30 @@ if __name__=='__main__':
             else: 
                 closest_match = estimated_match
             print(f"Video: {eye} basler frame number: {basler_frame_number}, eye video frame number: {closest_match}")
-            print(f"\tTime difference between basler and pupil is {basler_time - pupil_timestamps[closest_match]} ns")
+            time_difference = basler_time - pupil_timestamps[closest_match]
+            print(f"\tTime difference between basler and pupil is {time_difference} ns or {time_difference/1e9} s")
+            print(f"Basler time: {basler_time}, {eye} time {pupil_timestamps[closest_match]}")
+            frame_numbers.append(closest_match)
+            times.append(pupil_timestamps[closest_match])
+        print(f"\tTime difference between {eye} start and end is {(times[1] - times[0]) / 1e9} s")
+
+        clip_info[eye] = {
+            "start_frame": int(frame_numbers[0]), 
+            "end_frame": int(frame_numbers[1]),
+            "start_time_ns": int(times[0]),
+            "end_time_ns": int(times[1]),
+            "time_difference_start_ns": int(basler_median_starting_timestamp-times[0]),
+            "time_difference_end_ns": int(basler_median_ending_timestamp-times[1])
+        }
+
+    return clip_info
+
+
+if __name__=='__main__':
+    session_folder = Path("/home/scholl-lab/ferret_recordings/session_2025-07-01_ferret_757_EyeCameras_P33_EO5")
+
+    starting_basler_frame = 7200
+    ending_basler_frame = 12600
+
+
+    closest_pupil_frame_to_basler_frame(session_folder, starting_basler_frame, ending_basler_frame)
