3c0c7e96a4aada180fa11235b6bd5dbaf7df0d4b
bs_lab <info@freemocap.org>
2025-10-27 11:59:59 -0600

    save videos as part of eye analysis processing

--- python_code/eye_analysis/process_eye_session.py
+++ python_code/eye_analysis/process_eye_session.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pandas as pd
 
 from python_code.eye_analysis.data_processing.align_data.eye_anatomical_alignment import eye_alignment_main, merge_eye_output_csvs
+from python_code.eye_analysis.video_viewers.stabilized_eye_viewer import create_stabilized_eye_videos
 
 def merge_head_data(merged_eye_output: pd.DataFrame, head_data: Path) -> pd.DataFrame:
     head_data_csv = pd.read_csv(head_data)
@@ -18,6 +19,8 @@ def process_eye_session(
     eye_1_dlc_csv: Path,
     eye_0_timestamps_npy: Path,
     eye_1_timestamps_npy: Path,
+    eye_0_video_path: Path,
+    eye_1_video_path: Path,
     head_data: Path | None = None
 ):
     recording_name = session_folder.stem
@@ -44,26 +47,38 @@ def process_eye_session(
     merged_eye_output = merge_eye_output_csvs(eye_data_path=clip_folder / "eye_data")
     merged_eye_output.to_csv(clip_folder / "eye_data" / f"{recording_name}_{clip_name}_eye_data.csv", index=False)
 
-    if head_data:
-        head_and_eye_data = merge_head_data(merged_eye_output=merged_eye_output, head_data=head_data)
-        head_and_eye_data.to_csv(clip_folder / "eye_data" / f"{recording_name}_{clip_name}_head_and_eye_data.csv", index=False)
-
     # run video creation
+    create_stabilized_eye_videos(
+        base_path=clip_folder,
+        video_path=eye_0_video_path,
+        timestamps_npy_path=eye_0_timestamps_npy,
+        csv_path=eye_0_dlc_csv
+    )
+
+    create_stabilized_eye_videos(
+        base_path=clip_folder, 
+        video_path=eye_1_video_path,
+        timestamps_npy_path=eye_1_timestamps_npy,
+        csv_path=eye_1_dlc_csv
+    )
 
 if __name__ == "__main__":
-    session_folder = Path("/home/scholl-lab/ferret_recordings/session_2025-07-11_ferret_757_EyeCamera_P43_E15__1")
+    session_folder = Path("/home/scholl-lab/ferret_recordings/session_2025-07-01_ferret_757_EyeCameras_P33_EO5")
 
-    clip_name = "0m_37s-1m_37s"
+    clip_name = "1m_20s-2m_20s"
     clip_folder = session_folder / "clips" / clip_name
     
 
-    eye_0_dlc_csv = clip_folder / "eye_data" / "dlc_output" / "eye_model_v2_model_outputs_iteration_0_flipped" / f"eye0_clipped_4354_11523DLC_Resnet50_eye_model_v2_shuffle1_snapshot_020.csv"
-    eye_1_dlc_csv = clip_folder / "eye_data" / "dlc_output" / "eye_model_v2_model_outputs_iteration_0_flipped" / f"eye1_clipped_4371_11541_flippedDLC_Resnet50_eye_model_v2_shuffle1_snapshot_020.csv"
+    eye_0_dlc_csv = clip_folder / "eye_data" / "dlc_output" / "eye_model_v3_model_outputs_iteration_8_flipped" / f"eye0_clipped_9033_16234DLC_Resnet50_eye_model_v3_shuffle1_snapshot_130.csv"
+    eye_1_dlc_csv = clip_folder / "eye_data" / "dlc_output" / "eye_model_v3_model_outputs_iteration_8_flipped" / f"eye1_clipped_9033_16234_flippedDLC_Resnet50_eye_model_v3_shuffle1_snapshot_130.csv"
 
-    eye_0_timestamps_npy = clip_folder / "eye_data" / "eye_videos" / f"eye0_timestamps_utc_clipped_4354_11523.npy"
-    eye_1_timestamps_npy = clip_folder / "eye_data" / "eye_videos" / f"eye1_timestamps_utc_clipped_4371_11541.npy"
+    eye_0_timestamps_npy = clip_folder / "eye_data" / "eye_videos" / f"eye0_timestamps_utc_clipped_9033_16234.npy"
+    eye_1_timestamps_npy = clip_folder / "eye_data" / "eye_videos" / f"eye1_timestamps_utc_clipped_9033_16234.npy"
 
-    head_data = Path("/home/scholl-lab/ferret_recordings/session_2025-07-11_ferret_757_EyeCamera_P43_E15__1/clips/0m_37s-1m_37s/mocap_data/tidy_head_data.csv")
+
+    eye_0_video_path = clip_folder /"eye_data" / "eye_videos" / "flipped_eye_videos" / "eye0_clipped_9033_16234.mp4"
+    eye_1_video_path = clip_folder /"eye_data" / "eye_videos" / "flipped_eye_videos" / "eye1_clipped_9033_16234_flipped.mp4"
+
     process_eye_session(
         session_folder=session_folder,
         clip_folder=clip_folder,
@@ -72,5 +87,6 @@ if __name__ == "__main__":
         eye_1_dlc_csv=eye_1_dlc_csv,
         eye_0_timestamps_npy=eye_0_timestamps_npy,
         eye_1_timestamps_npy=eye_1_timestamps_npy,
-        head_data=head_data
+        eye_0_video_path=eye_0_video_path,
+        eye_1_video_path=eye_1_video_path
     )
