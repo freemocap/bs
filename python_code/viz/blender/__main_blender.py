4bfb25af0560a6a57595b52f4f14218eb4645504
Jon Matthis <jonmatthis@gmail.com>
2026-06-09 11:41:00 -0400

    init branch

---
+++ python_code/viz/blender/__main_blender.py
@@ -0,0 +1,14 @@
+from python_code.rigid_body_solver.reprojection_error import load_camera_calibration
+
+# Load recording data
+## Camera Calibration data (via FMC CameraModels)
+cameras = load_camera_calibration()
+## Synchronized Videos  (via FMC VideoGroup)
+
+## Trajectories
+### Skull&Spine
+### Toy
+
+## RigidBodies
+### Skull
+### Eyes
